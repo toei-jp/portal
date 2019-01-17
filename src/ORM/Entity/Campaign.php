e43a833095820cca273be7a359e626fd08a7b70d
Atsushi Okui <okui@motionpicture.jp>
2019-01-17 19:40:07 +0900

    キャンペーン情報の機能を移植

---
+++ src/ORM/Entity/Campaign.php
@@ -0,0 +1,275 @@
+<?php
+/**
+ * Campaign.php
+ * 
+ * @author Atsushi Okui <okui@motionpicture.jp>
+ */
+
+namespace Toei\Portal\ORM\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Campaign entity class
+ * 
+ * @ORM\Entity
+ * @ORM\Table(name="campaign", options={"collate"="utf8mb4_general_ci"})
+ * @ORM\HasLifecycleCallbacks
+ */
+class Campaign extends AbstractEntity
+{
+    use SavedUserTrait;
+    use SoftDeleteTrait;
+    use TimestampableTrait;
+    
+    /**
+     * id
+     * 
+     * @var int
+     * @ORM\Id
+     * @ORM\Column(type="integer", options={"unsigned"=true})
+     * @ORM\GeneratedValue
+     */
+    protected $id;
+    
+    /**
+     * title
+     *
+     * @var Title|null
+     * @ORM\ManyToOne(targetEntity="Title")
+     * @ORM\JoinColumn(name="title_id", referencedColumnName="id", nullable=true, onDelete="RESTRICT")
+     */
+    protected $title;
+    
+    /**
+     * image
+     *
+     * @var File
+     * @ORM\OneToOne(targetEntity="File", fetch="EAGER")
+     * @ORM\JoinColumn(name="image_file_id", referencedColumnName="id", nullable=false, onDelete="RESTRICT")
+     */
+    protected $image;
+    
+    /**
+     * name
+     * 
+     * @var string
+     * @ORM\Column(type="string")
+     */
+    protected $name;
+    
+    /**
+     * start_dt
+     *
+     * @var \DateTime
+     * @ORM\Column(type="datetime", name="start_dt")
+     */
+    protected $startDt;
+    
+    /**
+     * end_dt
+     *
+     * @var \DateTime
+     * @ORM\Column(type="datetime", name="end_dt")
+     */
+    protected $endDt;
+    
+    /**
+     * url
+     * 
+     * @var string
+     * @ORM\Column(type="string")
+     */
+    protected $url;
+    
+    /**
+     * pages
+     * 
+     * @var Collection
+     * @ORM\OneToMany(targetEntity="PageCampaign", mappedBy="campaign")
+     */
+    protected $pages;
+    
+    /**
+     * theaters
+     *
+     * @var Collection
+     * @ORM\OneToMany(targetEntity="TheaterCampaign", mappedBy="campaign")
+     */
+    protected $theaters;
+    
+    
+    /**
+     * construct
+     * 
+     * @throws \LogicException
+     */
+    public function __construct()
+    {
+        throw new \LogicException('Not allowed.');
+    }
+    
+    /**
+     * get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    
+    /**
+     * get title
+     *
+     * @return Title|null
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+    
+    /**
+     * set title
+     *
+     * @param Title|null $title
+     * @return void
+     * @throws \LogicException
+     */
+    public function setTitle($title)
+    {
+        throw new \LogicException('Not allowed.');
+    }
+    
+    /**
+     * get image
+     *
+     * @return File
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+    
+    /**
+     * set image
+     *
+     * @param File $image
+     * @return void
+     * @throws \LogicException
+     */
+    public function setImage(File $image)
+    {
+        throw new \LogicException('Not allowed.');
+    }
+    
+    /**
+     * get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+    
+    /**
+     * set name
+     *
+     * @param string $name
+     * @return void
+     * @throws \LogicException
+     */
+    public function setName(string $name)
+    {
+        throw new \LogicException('Not allowed.');
+    }
+    
+    /**
+     * get start_dt
+     *
+     * @return \DateTime
+     */
+    public function getStartDt()
+    {
+        return $this->startDt;
+    }
+    
+    /**
+     * set start_dt
+     *
+     * @param \DateTime|string $startDt
+     * @return void
+     * @throws \LogicException
+     */
+    public function setStartDt($startDt)
+    {
+        throw new \LogicException('Not allowed.');
+    }
+    
+    /**
+     * get end_dt
+     *
+     * @return \DateTime
+     */
+    public function getEndDt()
+    {
+        return $this->endDt;
+    }
+    
+    /**
+     * set end_dt
+     *
+     * @param \DateTime|string $endDt
+     * @return void
+     * @throws \LogicException
+     */
+    public function setEndDt($endDt)
+    {
+        throw new \LogicException('Not allowed.');
+    }
+    
+    /**
+     * get url
+     *
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+    
+    /**
+     * set url
+     *
+     * @param string $url
+     * @return void
+     * @throws \LogicException
+     */
+    public function setUrl(string $url)
+    {
+        throw new \LogicException('Not allowed.');
+    }
+    
+    /**
+     * get pages
+     *
+     * @return Collection
+     */
+    public function getPages(): Collection
+    {
+        return $this->pages;
+    }
+    
+    /**
+     * get theaters
+     *
+     * @return Collection
+     */
+    public function getTheaters(): Collection
+    {
+        return $this->theaters;
+    }
+}
