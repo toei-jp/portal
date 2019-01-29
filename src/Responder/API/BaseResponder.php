68e98a3bc5c93cfdfea33a60930dbe261f1355ad
Atsushi Okui <okui@motionpicture.jp>
2019-01-29 13:38:17 +0900

    APIのベースを実装

---
+++ src/Responder/API/BaseResponder.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * BaseResponder.php
+ * 
+ * @author Atsushi Okui <okui@motionpicture.jp>
+ */
+
+namespace Toei\Portal\Responder\API;
+
+use Toei\Portal\Responder\AbstractResponder;
+
+/**
+ * Base responder
+ */
+abstract class BaseResponder extends AbstractResponder
+{
+    /**
+     * contsruct
+     */
+    public function __construct()
+    {
+    }
+}
