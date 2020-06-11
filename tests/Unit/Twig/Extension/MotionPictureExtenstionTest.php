5e7074c74e19955249c7475e4489da96ec822b4b
Atsushi Okui <okui@motionpicture.jp>
2020-06-11 18:29:25 +0900

    Add Twig Extension tests

---
+++ tests/Unit/Twig/Extension/MotionPictureExtenstionTest.php
@@ -0,0 +1,155 @@
+<?php
+
+/**
+ * MotionPictureExtenstionTest.php
+ *
+ * @author Atsushi Okui <okui@motionpicture.jp>
+ */
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Twig\Extension;
+
+use Mockery;
+use Toei\Portal\Twig\Extension\MotionPictureExtenstion;
+
+/**
+ * MotionPicture extension test
+ */
+final class MotionPictureExtenstionTest extends BaseTestCase
+{
+    /**
+     * Create target mock
+     *
+     * @return \Mockery\MockInterface|\Mockery\LegacyMockInterface|MotionPictureExtenstion
+     */
+    protected function createTargetMock()
+    {
+        return Mockery::mock(MotionPictureExtenstion::class);
+    }
+
+    /**
+     * Create Target reflection
+     *
+     * @return \ReflectionClass
+     */
+    protected function createTargetReflection()
+    {
+        return new \ReflectionClass(MotionPictureExtenstion::class);
+    }
+
+    /**
+     * test construct
+     *
+     * @test
+     * @return void
+     */
+    public function testConstruct()
+    {
+        $targetMock = $this->createTargetMock();
+        $settings = [
+            'foo' => 'bar',
+        ];
+
+        // execute constructor
+        $targetRef = $this->createTargetReflection();
+        $constructorRef = $targetRef->getConstructor();
+        $constructorRef->invoke($targetMock, $settings);
+
+        // test property "settings"
+        $settingsPropertyRef = $targetRef->getProperty('settings');
+        $settingsPropertyRef->setAccessible(true);
+        $this->assertEquals(
+            $settings,
+            $settingsPropertyRef->getValue($targetMock)
+        );
+    }
+
+    /**
+     * test getApiEndpoint
+     *
+     * @test
+     * @return void
+     */
+    public function testGetApiEndpoint()
+    {
+        $targetMock = $this->createTargetMock();
+        $targetMock->makePartial();
+        $settings = [
+            'api_endpoint' => 'example.com/api',
+        ];
+
+        $targetRef = $this->createTargetReflection();
+        $settingsPropertyRef = $targetRef->getProperty('settings');
+        $settingsPropertyRef->setAccessible(true);
+        $settingsPropertyRef->setValue($targetMock, $settings);
+
+        $this->assertEquals($settings['api_endpoint'], $targetMock->getApiEndpoint());
+    }
+
+    /**
+     * test getWaiterServerUrl
+     *
+     * @test
+     * @return void
+     */
+    public function testGetWaiterServerUrl()
+    {
+        $targetMock = $this->createTargetMock();
+        $targetMock->makePartial();
+        $settings = [
+            'waiter_server_url' => 'https://example.com/waiter',
+        ];
+
+        $targetRef = $this->createTargetReflection();
+        $settingsPropertyRef = $targetRef->getProperty('settings');
+        $settingsPropertyRef->setAccessible(true);
+        $settingsPropertyRef->setValue($targetMock, $settings);
+
+        $this->assertEquals($settings['waiter_server_url'], $targetMock->getWaiterServerUrl());
+    }
+
+    /**
+     * test getTicketSiteUrl
+     *
+     * @test
+     * @return void
+     */
+    public function testGetTicketSiteUrl()
+    {
+        $targetMock = $this->createTargetMock();
+        $targetMock->makePartial();
+        $settings = [
+            'ticket_site_url' => 'https://example.com/ticket',
+        ];
+
+        $targetRef = $this->createTargetReflection();
+        $settingsPropertyRef = $targetRef->getProperty('settings');
+        $settingsPropertyRef->setAccessible(true);
+        $settingsPropertyRef->setValue($targetMock, $settings);
+
+        $this->assertEquals($settings['ticket_site_url'], $targetMock->getTicketSiteUrl());
+    }
+
+    /**
+     * test getProjectId
+     *
+     * @test
+     * @return void
+     */
+    public function testGetProjectId()
+    {
+        $targetMock = $this->createTargetMock();
+        $targetMock->makePartial();
+        $settings = [
+            'project_id' => 'aaabbbccc',
+        ];
+
+        $targetRef = $this->createTargetReflection();
+        $settingsPropertyRef = $targetRef->getProperty('settings');
+        $settingsPropertyRef->setAccessible(true);
+        $settingsPropertyRef->setValue($targetMock, $settings);
+
+        $this->assertEquals($settings['project_id'], $targetMock->getProjectId());
+    }
+}
---
+++ tests/Unit/Twig/Extension/ShowingFormatExtensionTest.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * ShowingFormatExtensionTest.php
+ *
+ * @author Atsushi Okui <okui@motionpicture.jp>
+ */
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Twig\Extension;
+
+use Mockery;
+use Toei\Portal\ORM\Entity\ShowingFormat;
+use Toei\Portal\Twig\Extension\ShowingFormatExtension;
+
+/**
+ * ShowingFormat extension test
+ */
+final class ShowingFormatExtensionTest extends BaseTestCase
+{
+    /**
+     * Create target mock
+     *
+     * @return \Mockery\MockInterface|\Mockery\LegacyMockInterface|ShowingFormatExtension
+     */
+    protected function createTargetMock()
+    {
+        return Mockery::mock(ShowingFormatExtension::class);
+    }
+
+    /**
+     * test getVoiceText
+     *
+     * @test
+     * @return void
+     */
+    public function testGetVoiceText()
+    {
+        $targetMock = $this->createTargetMock();
+        $targetMock->makePartial();
+
+        $this->assertEquals('字幕版', $targetMock->getVoiceText(ShowingFormat::VOICE_SUBTITLE));
+        $this->assertEquals('吹替版', $targetMock->getVoiceText(ShowingFormat::VOICE_DUB));
+        $this->assertEquals('', $targetMock->getVoiceText(0));
+    }
+}
