598ae69d5a1b090ad363a1b52112ee0b409cb433
Atsushi Okui <okui@motionpicture.jp>
2020-06-11 17:56:55 +0900

    設定を直接渡すように変更

--- config/container.php
+++ config/container.php
@@ -38,7 +38,7 @@ $container['view'] = function ($container) {
         $container->get('bc'),
         $container->get('settings')['storage']['public_endpoint']
     ));
-    $view->addExtension(new \Toei\Portal\Twig\Extension\MotionPictureExtenstion($container));
+    $view->addExtension(new \Toei\Portal\Twig\Extension\MotionPictureExtenstion($container->get('settings')['mp']));
     $view->addExtension(new \Toei\Portal\Twig\Extension\ShowingFormatExtension());
     $view->addExtension(new \Toei\Portal\Twig\Extension\TitleExtension());
 
